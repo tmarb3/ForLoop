15572235f4a742151ab1b840e29b6bbcdbb88ace
Thomas Marble <tmarble3@gmail.com>
2022-03-21 09:48:10 -0600

    Add project files.

---
+++ ForLoop/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");      
+            }
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+
+        }
+    }
+}
